1ec49d34d6c79e81465497eed7bfb2d007445bbd
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2012-10-01 16:09:29 -0400

    Add mkdir wrapper.

--- systemMisc.py
+++ systemMisc.py
@@ -1037,6 +1037,26 @@ def shellne(command):
     return data
 ## end of http://code.activestate.com/recipes/52296/ }}}
 
+def _mkdir(newdir):
+    """
+    works the way a good mkdir should :)
+        - already exists, silently complete
+        - regular file in the way, raise an exception
+        - parent directory(ies) does not exist, make them as well
+    """
+    if os.path.isdir(newdir):
+        pass
+    elif os.path.isfile(newdir):
+        raise OSError("a file with the same name as the desired " \
+                      "dir, '%s', already exists." % newdir)
+    else:
+        head, tail = os.path.split(newdir)
+        if head and not os.path.isdir(head):
+            _mkdir(head)
+        #print "_mkdir %s" % repr(newdir)
+        if tail:
+            os.mkdir(newdir)
+
     
 def file_exists(astr_fileName):
     try:
