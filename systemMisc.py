80d53dfb398a2552985d6a27a6dc7906ab4f8dfd
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2013-03-27 16:23:22 -0400

    Add case-like convenience class.

--- systemMisc.py
+++ systemMisc.py
@@ -1215,5 +1215,44 @@ def make_xlat(*args, **kwds):
     return rx.sub(one_xlat, text)
   return xlat
 
+# This class provides 'switch/case' syntax.
+# From: http://code.activestate.com/recipes/410692-readable-switch-construction-without-lambdas-or-di/
+class switch(object):
+    def __init__(self, value):
+        self.value = value
+        self.fall = False
 
+    def __iter__(self):
+        """Return the match method once, then stop"""
+        yield self.match
+        raise StopIteration
+
+    def match(self, *args):
+        """Indicate whether or not to enter a case suite"""
+        if self.fall or not args:
+            return True
+        elif self.value in args: # changed for v1.5, see below
+            self.fall = True
+            return True
+        else:
+            return False
+# HOWTO use:
+
+#v = 'ten'
+#for case in switch(v):
+    #if case('one'):
+        #print 1
+        #break
+    #if case('two'):
+        #print 2
+        #break
+    #if case('ten'):
+        #print 10
+        #break
+    #if case('eleven'):
+        #print 11
+        #break
+    #if case(): # default, could also just omit condition or 'if True'
+        #print "something else!"
+        ## No need to break here, it'll stop anyway
 
